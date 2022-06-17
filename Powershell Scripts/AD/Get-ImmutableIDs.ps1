c55791b8d63f7c021f84919827d39754a0ded8da
smillier <sylvain.millier@gmail.com>
2022-06-17 16:31:39 +0200

    Change to Get-ImmutableIDs

--- Powershell Scripts/AD/Get-ImmutableIDs.ps1
+++ Powershell Scripts/AD/Get-ImmutableIDs.ps1
@@ -1,4 +1,17 @@
 Import-Module ActiveDirectory
-$user = Get-ADUser -Identity [username]
-$immutableid = [System.Convert]::ToBase64String($user.ObjectGUID.tobytearray())
-$immutableid #(Ca sera la valeur à définir pour le ImmutableID dans Azure)
+$users = Get-ADUser -Filter * -SearchBase "OU=Users,DC=domain,DC=local"
+$exp_users = @()
+$i=0
+foreach ($user in $users)
+{
+    $users[$i].ObjectGUID
+    $item = New-Object PSObject
+    $item | Add-Member -MemberType NoteProperty -Name "userPrincipalName"  -Value $user.UserprincipalName
+    $id = [System.Convert]::ToBase64String($user.ObjectGUID.tobytearray())
+    $item | Add-Member -MemberType NoteProperty -Name "immutableID"  -Value $id.ToString()
+    $exp_users += $item
+    $i++
+}
+$exp_users | Export-Csv -Path c:\temp\usersimmutableIDs.csv
+#$immutableid = [System.Convert]::ToBase64String($user.ObjectGUID.tobytearray())
+#$immutableid #(Ca sera la valeur à définir pour le ImmutableID dans Azure)
