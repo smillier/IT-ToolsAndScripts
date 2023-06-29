8d515d16ea48990fc06fb46058b298cb28762665
smillier <sylvain.millier@gmail.com>
2023-06-29 10:01:59 +0200

    Add calendar  permissions script

---
+++ Powershell Scripts/Exchange/add-mailboxCalendarPermission.ps1
@@ -0,0 +1,7 @@
+foreach($user in Get-Mailbox -RecipientTypeDetails UserMailbox) {
+
+    $cal = $user.alias+":\Calendar"
+    
+    Add-MailboxFolderPermission -Identity $cal -User Grp.Reviewers -AccessRights Reviewer
+    Add-MailboxFolderPermission -Identity $cal -User Grp.Editors -AccessRights Editor
+}
